a5c9758df4c1c1bbe9f4f22aee976357972c6ec4
pqwy <unitedbiscuits@gmail.com>
2013-10-19 01:21:36 +0200

    fix for bytestring-0.9.2

--- code/haskell/src/pong.hs
+++ code/haskell/src/pong.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings  #-}
 
 import Control.Monad
-import System.ZMQ3
+
+import Data.ByteString.Char8 ()
 import Data.List.NonEmpty
+
+import System.ZMQ3
 import System.Environment
 
 main = do
