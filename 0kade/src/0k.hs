b231409f28eab083e416eb86c243f0704361b926
pqwy <unitedbiscuits@gmail.com>
2013-10-18 20:28:35 +0200

    parametri

--- 0kade/src/0k.hs
+++ 0kade/src/0k.hs
@@ -5,19 +5,26 @@ import Data.Monoid
 import Control.Monad.Trans
 import Data.Conduit
 import Data.Conduit.List
---  import Blaze.ByteString.Builder.Char.Utf8
 import Blaze.ByteString.Builder
 
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.HTTP.Types.Status
 
+import System.Timeout
+
+import ZeroOpts
 import PseudoMQ
 
 main = do
-  sr <- sudo_sandwich "tcp://127.0.0.1:10030"
-  run 1337 $ \req -> do
-    body <- requestBody req $$ (mconcat <$> consume)
-    rep  <- lift $ mconcat <$> sr [body]
-    return $ ResponseBuilder ok200 [] $ fromByteString rep
+  opt <- getOptions
+  sr  <- sudo_sandwich (o_zmq opt)
+  run (o_http opt) $ \req -> do
+    body <- requestBody req $$ mconcat `fmap` consume
+    rep  <- lift $ timeout (o_timeout' opt) $ mconcat `fmap` sr [body]
+    return $ maybe
+      (ResponseBuilder status408 [] $ fromByteString "")
+      (ResponseBuilder ok200 [] . fromByteString)
+      rep
 
+o_timeout' = floor . (* 1000000) . o_timeout
