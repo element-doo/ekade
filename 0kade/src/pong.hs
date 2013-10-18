691293a839b7a9c7138f9d2f18c057ccacc7b3d6
pqwy <unitedbiscuits@gmail.com>
2013-10-18 20:44:40 +0200

    more parametri

--- 0kade/src/pong.hs
+++ 0kade/src/pong.hs
@@ -3,13 +3,20 @@
 import Control.Monad
 import System.ZMQ3
 import Data.List.NonEmpty
+import System.Environment
 
 main = do
-  mq  <- context
-  skt <- socket mq Rep
-  connect skt "tcp://127.0.0.1:10030"
-  forever $ do
-    msg <- receiveMulti skt
-    sendMulti skt $ "PONG " :| msg
+  addr <- getAddr
+  putStrLn $ "using " ++ addr
 
+  sock <- context >>= (`socket` Rep)
+  connect sock addr
+
+  forever $ receiveMulti sock >>= sendMulti sock . ("PONG " :|)
+
+getAddr = do
+  args <- getArgs
+  return $ case args of
+    addr:_ -> addr
+    _      -> "tcp://127.0.0.1:10030"
 
