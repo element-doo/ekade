4c16cf9da25341b4af664d7b6e745a770398fb10
Tomo Ferega <tomo@element.hr>
2013-10-19 23:28:47 +0200

    Listener now listenes for tubs.

--- code/scala/Api/src/main/scala/com/emajliramokade/server/api/Locator.scala
+++ code/scala/Api/src/main/scala/com/emajliramokade/server/api/Locator.scala
@@ -3,11 +3,12 @@ package server
 package api
 
 import rest.RestListener
-
 import hr.ngs.patterns.{DependencyContainer, IServiceLocator}
 import net.liftweb.http.rest.RestHelper
 import org.slf4j.LoggerFactory
 import scala.reflect.runtime.universe.TypeTag
+import hr.ngs.patterns.JsonSerialization
+import hr.ngs.patterns.ISerialization
 
 object Locator extends IServiceLocator {
   private val container = {
@@ -16,6 +17,7 @@ object Locator extends IServiceLocator {
     new DependencyContainer()
       .register[org.slf4j.Logger](logger)
       .register[RestListener, RestHelper]
+      .register[JsonSerialization, ISerialization[String]]
   }
 
   def resolve[T: TypeTag] =
