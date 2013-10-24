2b449b22c898aeadcf5c350e200c2c4ca8ee3c41
Tomo Ferega <tomo@element.hr>
2013-10-24 06:35:53 +0200

    RemotingDispatch now accepts a HTTP method.

--- code/scala/Services/src/main/scala/com/emajliramokade/services/RemotingDispatch.scala
+++ code/scala/Services/src/main/scala/com/emajliramokade/services/RemotingDispatch.scala
@@ -5,8 +5,10 @@ import dispatch._
 import scala.concurrent.Future
 
 trait RemotingDispatch[T] extends Remoting[T] {
+  def method: String
+
   def send(serviceUrl: String, request: Array[Byte]): Future[Array[Byte]] = {
-    val req = url(serviceUrl).setBody(request)
+    val req = url(serviceUrl).setBody(request).setMethod(method)
 
     Http(req) map { response =>
       response.getResponseBodyAsBytes()
--- code/scala/Services/src/main/scala/com/emajliramokade/services/impl/LaravelRemoteEmailValidator.scala
+++ code/scala/Services/src/main/scala/com/emajliramokade/services/impl/LaravelRemoteEmailValidator.scala
@@ -10,5 +10,6 @@ class LaravelRemoteEmailValidator(
     val serialization: ISerialization[String])
     extends abstracts.RemoteEmailValidator
     with    RemotingDispatch[Zahtjev] {
+  val method = "POST"
   def serviceUrlFactory(t: Zahtjev) = "http://emajliramokade.com:10070/zahtjev-check"
 }
